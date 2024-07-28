6aab6bc437349c68b76422cad82558208e264f2e
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-07-28 02:32:37 -0700

    set up server.js

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const api_routes = require('./routes/api-routes')
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(express.static("public"));
+app.use(html_routes)
+app.use(ape_routes)
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
