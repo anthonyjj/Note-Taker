ce4716ca708e6abe064df082eda393cececcaeae
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-07-28 03:16:01 -0700

    fixed a syntax error in api routes that made my notes not save (forgot to put / in front of api/notes)

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,32 +1,33 @@
-const router = require ('express').Router();
-const {v4: uuidv4} = require('uuid');
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const fs = require ("fs");
 
-router.get('api/notes', async(req, res) => {
-    const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
-    res.json(dbJson);
+
+router.get('/api/notes', async (req, res) => {
+  const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  res.json(dbJson);
 });
 
 router.post('/api/notes', (req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
-    const newFeedback = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4(),
-    };
-    dbJson.push(newFeedback);
-    fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
-    res.json(dbJson);
+  const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+  const newFeedback = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  dbJson.push(newFeedback);
+  fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
+  res.json(dbJson);
 });
 
-router.delete('api/notes/:id', (req, res) => {
-    let data = fs.readFileSync("db/db.json","utf8");
-    const dataJson = JSON.parse(data);
-    const newNotes= dataJson.filter((notte) => {
-        return notEqual.id !== req.params.id;
-    });
-    fs.writeFileSync("db/db.json",JSON.stringify(newNotes));
-    res.json("Note deleted.");
+router.delete('/api/notes/:id', (req, res) => {
+  let data = fs.readFileSync("db/db.json", "utf8");
+  const dataJSON =  JSON.parse(data);
+  const newNotes = dataJSON.filter((note) => { 
+    return note.id !== req.params.id;
+  });
+  fs.writeFileSync("db/db.json",JSON.stringify(newNotes));
+  res.json("Note deleted.");
 });
 
-module.exports = router;
+module.exports = router; 
